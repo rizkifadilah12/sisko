7d2467313c53716901f5b18107d8f3054f91d98e
rizkifadilah12 <rf8740072@gmail.com>
2024-02-01 19:46:08 +0700

    change product card

--- src/components/product/ProductComponent.jsx
+++ src/components/product/ProductComponent.jsx
@@ -22,50 +22,65 @@ const ProductComponent = () => {
   }, [dispatch]);
   return (
     <div className="">
-      {isLoading ? <ProductLoad/> :  
-      <Container className="mt-5">
-        <Row className="justify-content-md-center">
-          {product?.map((prod) => (
-            <Col key={prod.id} md="auto text-center">
-              {" "}
-              <Card style={{ width: "18rem" }}>
-                <Link to={`/detail/${prod.id}`}>
-                
-                <Card.Img variant="top" src={prod.photo} />
-                </Link>
-                <Card.Body>
-                  <Card.Title className="title-product">{prod.name}</Card.Title>
-                  <Card.Text className="price-product">Rp. {prod.price}</Card.Text>
-                  <Card.Text>
-                    {prod.plain_varian.map((variant) => (
-                    <DropdownButton
-                      id="dropdown-button-dark-example2"
-                      variant="light"
-                      size="sm"
-                      key={variant.id}
-                      title={variant.value}
-                      className="mt-2"
-                      data-bs-theme="light"
-                    >
-                      <Dropdown.Item href="#/action-1" className="variant-product" active>
-                        {variant.value}   {`(${prod.stock})`}
-                      </Dropdown.Item>
-                    </DropdownButton>
-                         ))}
-                  </Card.Text>
-                  <Link className="button">ADD TO CARD</Link>
-                </Card.Body>
-              </Card>
+      {isLoading ? (
+        <ProductLoad />
+      ) : (
+        <Container className="mt-5">
+          <Row className="justify-content-md-center">
+            {product?.map((prod) => (
+              <Col key={prod.id} md="auto text-center">
+                {" "}
+                <Card
+                  style={{ width: "18rem", height: "500px" }}
+                  className="card-product"
+                >
+                  <Link to={`/detail/${prod.id}`}>
+                    <Card.Img variant="top" src={prod.photo} />
+                  </Link>
+                  <Card.Body>
+                    <Card.Title className="title-product">
+                      {prod.name}
+                    </Card.Title>
+                    <Card.Text className="price-product">
+                      Rp. {prod.price}
+                    </Card.Text>
+                    <Card.Text>
+                      {prod.plain_varian.map((variant) => (
+                        <DropdownButton
+                          id="dropdown-button-dark-example2"
+                          variant="light"
+                          size="sm"
+                          key={variant.id}
+                          title={variant.value}
+                          className="mt-2"
+                          data-bs-theme="light"
+                        >
+                          <Dropdown.Item
+                            href="#/action-1"
+                            className="variant-product"
+                            active
+                          >
+                            {variant.value}
+                            {`(${prod.stock})`}
+                          </Dropdown.Item>
+                        </DropdownButton>
+                      ))}
+                    </Card.Text>
+                    <Link className="button">ADD TO CARD</Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+          <Row className="justify-content-md-center mt-2">
+            <Col md="auto text-center">
+              <Button variant="warning" className="rounded-pill">
+                Load More
+              </Button>
             </Col>
-          ))}
-        </Row>
-        <Row className="justify-content-md-center mt-2">
-          <Col md="auto text-center">
-            <Button variant="warning" className="rounded-pill">Load More</Button>
-          </Col>
-        </Row>
-      </Container>
-}
+          </Row>
+        </Container>
+      )}
     </div>
   );
 };
